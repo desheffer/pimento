abf3bc19bee5c5073768a71ea338cc648e643de5
Doug Sheffer <desheffer@gmail.com>
2023-10-27 01:29:44 -0400

    Add dbg! macro

--- kernel/src/print.rs
+++ kernel/src/print.rs
@@ -32,3 +32,21 @@ macro_rules! println {
         $crate::print::_print(format_args_nl!($($arg)*));
     }};
 }
+
+#[macro_export]
+macro_rules! dbg {
+    () => {
+        $crate::println!("[{}:{}]", file!(), line!())
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            tmp => {
+                $crate::println!("[{}:{}] {} = {:#?}", file!(), line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::dbg!($val)),+,)
+    };
+}
