75700e97435d1d4c3c3a52b4f205350f10b60cb7
Doug Sheffer <desheffer@gmail.com>
2023-11-05 19:07:01 -0500

    Enable access to boot parameters

--- kernel/src/boot/aarch64/start.s
+++ kernel/src/boot/aarch64/start.s
@@ -3,37 +3,37 @@
 .globl _start
 _start:
     // Get the range of BSS.
-    ldr x0, =__bss_start
-    ldr x1, =__bss_end
+    ldr x9, =__bss_start
+    ldr x10, =__bss_end
 
     // Zero out BSS.
 loop_clear_bss:
-    stp xzr, xzr, [x0], #16
-    cmp x0, x1
+    stp xzr, xzr, [x9], #16
+    cmp x9, x10
     blo loop_clear_bss
 
     // Enable AArch64 in EL1.
-    ldr x0, ={HCR_EL2_RW}
-    mrs x1, hcr_el2
-    orr x0, x0, x1
-    msr hcr_el2, x0
+    ldr x9, ={HCR_EL2_RW}
+    mrs x10, hcr_el2
+    orr x9, x9, x10
+    msr hcr_el2, x9
     isb
 
     // Change execution level to EL1h.
-    ldr x0, ={SPSR_EL2_INIT}
-    msr spsr_el2, x0
-    adr x0, enter_el1
-    msr elr_el2, x0
+    ldr x9, ={SPSR_EL2_INIT}
+    msr spsr_el2, x9
+    adr x9, enter_el1
+    msr elr_el2, x9
     eret
 
 enter_el1:
     // Set up initial stack pointer.
-    ldr x0, =__stack_start
-    mov sp, x0
+    ldr x9, =__stack_start
+    mov sp, x9
 
     // Set up vector table.
-    adr x0, vector_table
-    msr vbar_el1, x0
+    adr x9, vector_table
+    msr vbar_el1, x9
     isb
 
     b kernel_init
